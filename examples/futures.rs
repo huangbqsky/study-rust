5599523dc9587859c07e7a81736ac9b9df4e27b3
huangbqsky <huangbqsky@gmail.com>
2023-03-28 21:43:21 +0800

    SimpleFutrue

--- examples/futures.rs
+++ examples/futures.rs
@@ -34,7 +34,7 @@ impl SimpleSocket for Socket {
 
     fn read_buf(&self) -> Vec<u8> {
         let mut v = Vec::new();
-        v.push(self.bytesget); // 模拟 read
+        v.push(12); // 模拟 read
         v
     }
 }
