d66d66037ab3b84523b159c5ff7b3593511dd5e2
huangbqsky <huangbqsky@gmail.com>
2023-04-09 17:05:44 +0800

    tokio_mutex例子

--- examples/tokio_mutex.rs
+++ examples/tokio_mutex.rs
@@ -1,5 +1,6 @@
+#![allow(unused_imports, unused, dead_code)]
+
 use std::sync::Arc;
-#[allow(unused_imports, unused)]
 use tokio::{self, sync::{self, Mutex}, runtime::Runtime, time::{self, Duration}};
 
 fn main() {
@@ -9,6 +10,7 @@ fn main() {
     rt.block_on(std_mutex());
 
     rt.block_on(cross_await());
+    rt.block_on(cross_await_std_mutex());
 }
 
 async fn tokio_mutex(){
@@ -74,3 +76,51 @@ async fn cross_await(){
 
     time::sleep(Duration::from_secs(1)).await;
 }
+
+
+/**
+ * 继续使用标准库的Mutex： 这种方案的主要思想是让子任务和父任务不要出现不安全的数据交叉
+ *方案1. 可以在子任务await之前，把所有未实现Send的数据都drop掉，保证子任务无法引用父任务中的任何非Send数据。
+ */
+async fn add_drop_std_mutex(mutex: &std::sync::Mutex<u64>) {
+    {
+      let mut lock = mutex.lock().unwrap();
+      *lock += 1;
+    }
+    // 子任务，跨await，不引用父任务中的数据
+    time::sleep(Duration::from_millis(10)).await;
+}
+
+/**
+ * 继续使用标准库的Mutex：
+ * 方案2：将子任务sleep().await从这个函数中移走。
+ */
+async fn add_std_mutex(mutex: &std::sync::Mutex<u64>) -> u64 {
+    let mut lock = mutex.lock().unwrap();
+    *lock += 1;
+    *lock
+}  // 申请的互斥锁在此被释放
+
+/**
+ * tokio的Mutex性能相对较差一些，因此可以不使用tokio Mutex的情况下，尽量不使用它。
+ * 可以继续使用标准库的Mutex，但需要做一些调整:
+ * 方案1. 可以在子任务await之前，把所有未实现Send的数据都drop掉，保证子任务无法引用父任务中的任何非Send数据。
+ * 方案2. 将子任务sleep().await从这个函数中移走。
+ * 
+ */
+async fn cross_await_std_mutex(){
+    let mutex = Arc::new(std::sync::Mutex::new(0));
+
+    for i in 0..100 {
+        let lock = mutex.clone();
+        tokio::spawn(async move {
+            let n = add_std_mutex(&lock).await;
+            time::sleep(Duration::from_millis(n)).await;
+            println!("task: {}, data: {}", i, n);
+        });
+    }
+
+    time::sleep(Duration::from_secs(1)).await;
+    println!("data: {}", mutex.lock().unwrap());
+}
+
