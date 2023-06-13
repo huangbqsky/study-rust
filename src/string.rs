346a1a308b314e4ab21f6276d124a30ef0c27c6b
huangbqsky <huangbqsky@gmail.com>
2023-06-13 21:44:49 +0800

    rust string操作

--- src/string.rs
+++ src/string.rs
@@ -72,4 +72,60 @@ pub fn main() {
     string_clear.clear();
     dbg!(string_clear);
 
+    // 连接: 
+    // 1. 使用 + 或者 += 连接字符串, 要求右边的参数必须为字符串的切片引用（Slice)类型
+    // 其实当调用 + 的操作符时，相当于调用了 std::string 标准库中的 add() 方法，这里 add() 方法的第二个参数是一个引用的类型。
+    // 因此我们在使用 +， 必须传递切片引用类型。不能直接传递 String 类型。
+    // + 和 += 都是返回一个新的字符串。所以变量声明可以不需要 mut 关键字修饰
+
+    let string_append = String::from("hello ");
+    let string_rust = String::from("rust");
+    // &string_rust会自动解引用为&str
+    let result = string_append + &string_rust;
+    let mut result = result + "!";
+    result += "!!!";
+
+    println!("连接字符串 + -> {}", result);
+
+    // 2. 使用 format! 连接字符串,format! 这种方式适用于 String 和 &str 。
+    let s1 = "hello";
+    let s2 = String::from("rust");
+    let s = format!("{} {}!", s1, s2);
+    println!("format格式化字符串：{}", s);
+
+    // 字符串转义， 通过转义的方式 \ 输出 ASCII 和 Unicode 字符。
+      // 通过 \ + 字符的十六进制表示，转义输出一个字符，\\保持字符串原样；\3F -> ？
+      let byte_escape = "I'm writing \x52\x75\x73\x74!";
+      println!("What are you doing\x3F (\\x3F means ?) {}", byte_escape);
+  
+      // \u 可以输出一个 unicode 字符
+      let unicode_codepoint = "\u{211D}";
+      let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+  
+      println!(
+          "字符串转义：Unicode character {} (U+211D) is called {}",
+          unicode_codepoint, character_name
+      );
+  
+      // 换行了也会保持之前的字符串格式
+      let long_string = "String literals
+                          can span multiple lines.
+                          The linebreak and indentation here ->\
+                          <- can be escaped too!";
+      println!("保持之前的字符串格式： {}", long_string);
+
+      // 字符： 如果你想要以 Unicode 字符的方式遍历字符串，最好的办法是使用 chars 方法
+      for c in "中国人".chars() {
+        println!("{}", c);
+      }
+
+      // 字节：这种方式是返回字符串的底层字节数组表现形式
+      for b in "中国人".bytes() {
+        println!("{}", b);
+      }
+
+      ///总结：
+      // 1。字符串切片索引要注意，必须落在字符边界
+      // 2. push，insert，replace_range， pop，remove，truncate，clear 修改原有字符串，即字符串必须是 mut
+      // 3. replace，replacen，catenate, format!返回新的字符串，不用 mut。
 }
